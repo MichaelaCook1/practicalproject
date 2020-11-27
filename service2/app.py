ac7a0345ba402551f6fd1abe8a393ac5ea905104
MichaelaCook1 <MCook1@qa.com>
2020-11-27 15:14:54 +0000

    adding initial app unctionality

---
+++ service2/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, Response, request, jsonify
+import random
+
+app = Flask(__name__)
+
+@app.route('/d20', methods=['GET'])
+def d20():
+    d20 = []
+    d20 = random.randint(1, 20)
+    d20.append(d20)
+    return response(d20, mimetype="text/plain")
+
+if __name__ =="__main__":
+    app.run(debug=True, host='0.0.0.0', port=5001)
