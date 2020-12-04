8e57b33883869735c105b8fc3c576da6d9548b1a
MichaelaCook1 <MCook1@qa.com>
2020-12-04 10:20:17 +0000

    update

--- Jenkins/requirements.sh
+++ Jenkins/requirements.sh
@@ -2,13 +2,13 @@
 sudo apt update && sudo apt install -y curl jq
 sudo apt-get install python-pip -y
 sudo apt-get install python3-pip -y
-pip install pytest
-pip install pytest-cov
-pip install requests
-pip install flask
-pip install flask_sqlalchemy
-pip install flask_wtf
-pip install flask-requests
-pip install flask-response
-pip install pymysql
-pip install Werkzeug
+pip3 install pytest
+pip3 install pytest-cov
+pip3 install requests
+pip3 install flask
+pip3 install flask_sqlalchemy
+pip3 install flask_wtf
+pip3 install flask-requests
+pip3 install flask-response
+pip3 install pymysql
+pip3 install Werkzeug
