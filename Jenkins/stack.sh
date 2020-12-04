8054f1b02d32d08c86c453b2c0342d926ac400d9
michaelacook1 <mcook1@qa.com>
2020-12-04 22:41:08 +0000

    update

--- Jenkins/stack.sh
+++ Jenkins/stack.sh
@@ -3,6 +3,6 @@ docker pull michaelacook1/serv1i:latest
 docker pull michaelacook1/serv2i:latest
 docker pull michaelacook1/serv3i:latest
 docker pull michaelacook1/serv4i:latest
-cd /home/a_cook8757/PracticalProject
+cd PracticalProject/
 docker stack deploy --compose-file docker-compose.yaml app
 EOF
