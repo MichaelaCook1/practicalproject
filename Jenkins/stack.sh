cabd25db8e5e3bfab8a34708a486bec632a1d060
michaelacook1 <mcook1@qa.com>
2020-12-05 17:25:33 +0000

    update

--- Jenkins/stack.sh
+++ Jenkins/stack.sh
@@ -1,8 +1,9 @@
-ssh jenkins@swarm-master << EOF
-docker pull michaelacook1/serv1i:latest
-docker pull michaelacook1/serv2i:latest
-docker pull michaelacook1/serv3i:latest
-docker pull michaelacook1/serv4i:latest
+ssh swarm-master << EOF
+docker pull michaelacook1/service1:latest
+docker pull michaelacook1/service2:latest
+docker pull michaelacook1/service3:latest
+docker pull michaelacook1/service4:latest
+docker pull nginx:alpine
 git clone https://github.com/MichaelaCook1/practicalproject.git
 cd practicalproject/
 docker stack deploy --compose-file docker-compose.yaml app
