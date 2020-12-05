c25bdcfe1573819443110f0fc4f1140da46fbe34
michaelacook1 <mcook1@qa.com>
2020-12-05 17:34:31 +0000

    nginx failed continuing stack without

--- Jenkins/stack.sh
+++ Jenkins/stack.sh
@@ -3,7 +3,6 @@ docker pull michaelacook1/service1:latest
 docker pull michaelacook1/service2:latest
 docker pull michaelacook1/service3:latest
 docker pull michaelacook1/service4:latest
-docker pull nginx:alpine
 git clone https://github.com/MichaelaCook1/practicalproject.git
 cd practicalproject/
 docker stack deploy --compose-file docker-compose.yaml app
