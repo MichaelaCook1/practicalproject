4fc4c631625ec76155ad7969e33f3d6cbf22fc69
michaelacook1 <mcook1@qa.com>
2020-12-07 08:19:33 +0000

    finished testing reports

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -5,9 +5,9 @@ from os import getenv
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@34.89.21.101/attempts'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@34.89.119.24/attempts'
 app.config['SECRET_KEY'] = 'secretkey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
 
-from application import routes
+
--- service1/testing/test_unit.py
+++ service1/testing/test_unit.py
@@ -3,33 +3,13 @@ from flask import url_for
 from flask_testing import TestCase
 from app import app
 from application import db
+from application.models import __init__
 from application.models import attempts
+
 
 class TestBase(TestCase):
     def create_app(self):
-        app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///"
-        app.config['SECRET_KEY']='TEST_SECRET_KEY'
-        app.config['DEBUG']=True
         return app
-    
-    def setUp(self):
-        db.create_all()
-        id1 = attempts(id='1')
-        value1 = attempts(value='12')
-        result1 = attempts(value= 'Win')
-        db.session.add(id1)
-        db.session.add(value1)
-        db.session.add(result1)
-        db.session.commit()
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
-class TestIndex(TestBase):
-    def test_indexpage(self):
-        response = self.client.get(url_for('index'))
-        self.assertEqual(response.status_code, 200)
-
 
 class TestResponse(TestBase):
     def test_rollwin(self):
